bf73c59e7e27f84c4c7885050851729a3939b662
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-25 21:20:58 +0900

    [add] ルートの設定

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:using_riverpod_todo_app/firebase_options.dart';
 import 'package:using_riverpod_todo_app/ui/screen/auth_gate.dart';
+import 'package:using_riverpod_todo_app/ui/screen/login_screen.dart';
+import 'package:using_riverpod_todo_app/ui/screen/todo_list_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,6 +25,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      routes: {
+        '/login': (context) => const LoginScreen(),
+        '/todo_list': (context) => const TodoListScreen(),
+      },
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
