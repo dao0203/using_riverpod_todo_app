969851e3b48ea5cc464d0dac1a9dd52c258e99bf
佐藤 佑哉 <y-sato@sstn.jp>
2023-05-30 20:56:36 +0900

    [add] データソースとRepositoryクラスの実装クラスを作成

---
+++ lib/data/repository/data_source_repository_impl.dart
@@ -0,0 +1,34 @@
+import 'data_source_repository.dart';
+
+class DataSourceRepositoryImpl extends DataSourceRepository {
+  @override
+  Future<String> getUser() {
+    // TODO: implement getUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isSignedIn() {
+    // TODO: implement isSignedIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signIn(String email, String password) {
+    // TODO: implement signIn
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<String> signUp(String email, String password) {
+    // TODO: implement signUp
+    throw UnimplementedError();
+  }
+  
+}
---
+++ lib/data/source/account_service_impl.dart
@@ -0,0 +1,51 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:using_riverpod_todo_app/providers/auth_state_provider.dart';
+
+import 'account_service.dart';
+
+class accountSourceImpl extends AccountService {
+  final FirebaseAuth _auth;
+  final FirebaseFirestore _firestore;
+  accountSourceImpl(this._auth, this._firestore);
+  @override
+  Future<String> getUser() {
+    // TODO: implement getUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Stream<void> isSignedIn() => _auth.authStateChanges();
+
+  @override
+  Future<void> signIn(String email, String password) async {
+    // TODO: implement signIn
+    await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  Future<void> signOut() {
+    // TODO: implement signOut
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> signUp(
+    String email,
+    String password,
+    String githubId,
+  ) =>
+      _auth
+          .createUserWithEmailAndPassword(
+            email: email,
+            password: password,
+          )
+          .then(
+            (value) => _firestore.collection('users').doc(value.user!.uid).set(
+              {'githubId': githubId},
+            ),
+          );
+}
