3227a6712ec3d272979e92c1d33b9cc0198097dd
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-02 15:04:04 +0900

    [add] add Firebase Auth State Notifier

---
+++ lib/ui/state/auth_notofier.dart
@@ -0,0 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../data/repository/data_source_repository.dart';
+
+class AuthNotifier extends StateNotifier<User?> {
+  AuthNotifier(this._auth) : super(null) {
+    isSignedIn().listen((user) {
+      state = user;
+    });
+  }
+
+  final DataSourceRepository _auth;
+
+  Stream<User?> isSignedIn() {
+    return _auth.isSignedIn();
+  }
+
+  Future<void> signIn(String email, String password) async {
+    await _auth.signIn(email, password);
+  }
+
+  Future<void> signUp(String email, String password, String githubId) async {
+    await _auth.signUp(email, password, githubId);
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+  }
+
+  User getUser() {
+    return _auth.getUser();
+  }
+}
