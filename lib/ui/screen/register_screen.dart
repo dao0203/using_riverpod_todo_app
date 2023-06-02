1185952d2094ff9282e8764077a557bcf4452c1e
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-02 23:49:53 +0900

    [update] 新規登録画面のUIを作成

--- lib/ui/screen/register_screen.dart
+++ lib/ui/screen/register_screen.dart
@@ -1,20 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-
 import '../../utils/const.dart';
 
 class RegisterScreen extends HookConsumerWidget {
-  RegisterScreen({super.key});
-  final emailController = useTextEditingController();
-  final passwordController = useTextEditingController();
-  final githubIdController = useTextEditingController();
-  final registerKey = GlobalKey<FormState>();
+  const RegisterScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    throw Scaffold(
+    final emailController = useTextEditingController();
+    final passwordController = useTextEditingController();
+    final confirmPasswordController = useTextEditingController();
+    final githubIdController = useTextEditingController();
+    final registerKey = GlobalKey<FormState>();
+    return Scaffold(
       appBar: AppBar(
         title: const Text("新規登録"),
       ),
@@ -24,7 +23,66 @@ class RegisterScreen extends HookConsumerWidget {
           padding: padding8,
           child: Column(
             children: [
-              
+              brank16,
+              const Text("新規登録"),
+              brank16,
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'メールアドレスを入力してください';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  icon: Icon(Icons.email),
+                ),
+                controller: emailController,
+              ),
+              brank8,
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'パスワードを入力してください';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  icon: Icon(Icons.lock),
+                ),
+                controller: passwordController,
+              ),
+              brank8,
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'パスワードを入力してください';
+                  } else if (value != passwordController.text) {
+                    return 'パスワードが一致しません';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Password(確認用)',
+                  icon: Icon(Icons.lock),
+                ),
+                controller: confirmPasswordController,
+              ),
+              brank8,
+              TextFormField(
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Github IDを入力してください';
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Github ID',
+                  icon: Icon(Icons.lock),
+                ),
+                controller: githubIdController,
+              ),
             ],
           ),
         ),
