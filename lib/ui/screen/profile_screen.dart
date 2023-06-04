43fdb397296af42257746a1ede44052e7f0bc0d0
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-04 17:40:46 +0900

    [update] Github APIから取得してきたデータを一部表示

--- lib/ui/screen/profile_screen.dart
+++ lib/ui/screen/profile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:using_riverpod_todo_app/providers.dart';
 
 import '../../utils/const.dart';
 
@@ -9,6 +10,7 @@ class ProfileScreen extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final profile = ref.watch(profileProvider);
     return Scaffold(
       appBar: AppBar(
         //バックボタンにカラーをつける
@@ -27,28 +29,32 @@ class ProfileScreen extends HookConsumerWidget {
         child: Center(
           child: Padding(
             padding: padding8,
-            child: Column(
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(100.0),
-                  child: Image.network(
-                    'https://github.com/dao0203.png',
-                    fit: BoxFit.cover,
-                    width: 200,
-                    height: 200,
+            child: profile.when(
+              data: (data) => Column(
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(100.0),
+                    child: Image.network(
+                      'https://github.com/${data.login}.png',
+                      fit: BoxFit.cover,
+                      width: 200,
+                      height: 200,
+                    ),
                   ),
-                ),
-                brank16,
-                //名前を模したアイコン
-                const Text(
-                  "佐藤佑哉",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
+                  brank16,
+                  //名前を模したアイコン
+                  const Text(
+                    "佐藤佑哉",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                brank16,
-              ],
+                  brank16,
+                ],
+              ),
+              error: (error, stackTrace) => const Text("エラーが発生しました"),
+              loading: () => const CircularProgressIndicator(),
             ),
           ),
         ),
