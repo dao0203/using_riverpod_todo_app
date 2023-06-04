eaa9789b87a9f969b74e756bf2a421ac5bf599d6
佐藤 佑哉 <y-sato@sstn.jp>
2023-06-04 16:22:52 +0900

    [add] profileを取得するProviderを追加

--- lib/providers.dart
+++ lib/providers.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:using_riverpod_todo_app/ui/state/auth_notofier.dart';
 
+import 'data/model/github_profile.dart';
 import 'data/repository/data_source_repository.dart';
 import 'data/repository/data_source_repository_impl.dart';
 import 'data/source/account_service.dart';
@@ -12,14 +13,23 @@ final firebaseAuthProvider = Provider((ref) => FirebaseAuth.instance);
 final firebaseFirestoreProvider = Provider((ref) {
   return FirebaseFirestore.instance;
 });
+
+//DataSource
 final accountServiceProvider = Provider<AccountService>((ref) {
   return AccountServiceImpl(
       ref.watch(firebaseAuthProvider), ref.watch(firebaseFirestoreProvider));
 });
+
+//Repository
 final dataSourceRepositoryProvider = Provider<DataSourceRepository>((ref) {
   return DataSourceRepositoryImpl(ref.watch(accountServiceProvider));
 });
 
+final profileProvider = FutureProvider<GithubProfile>((ref) async{
+  return await ref.watch(dataSourceRepositoryProvider).getProfile();
+});
+
+//状態管理
 final authStateProvider = StateNotifierProvider<AuthNotifier, User?>((ref) {
   return AuthNotifier(ref.watch(dataSourceRepositoryProvider));
 });
